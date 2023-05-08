89cee994007b531ee2be5440d485c18f1f3e4582
Paul Applegate <paul@theapplegates.com>
2023-05-08 17:26:43 +0000

    new post, images added

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['paulapplegate.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
-
+staticImage: true,
 module.exports = withContentlayer(nextConfig);
