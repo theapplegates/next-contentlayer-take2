95b0772140951b51b837da1ba8db2eb35c6aad51
Paul <paul@theapplegates.com>
2023-06-04 23:01:24 -0400

    font change to Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
 import "./globals.css"
-import { Roboto_Flex } from "next/font/google";
+import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
 import { ModeToggle } from "@/components/mode-toggle"
 
-const roboto_flex = Roboto_Flex({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${roboto_flex.className}`}
+        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4">
